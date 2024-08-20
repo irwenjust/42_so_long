5827fa5a080980803a130ace170a44101735ffb2
irwenjust <35918898+irwenjust@users.noreply.github.com>
2024-08-20 09:59:29 +0300

    Fixed 'PEC' problem

--- src/move_player.c
+++ src/move_player.c
@@ -3,38 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   move_player.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: likong <likong@student.42.fr>              +#+  +:+       +#+        */
+/*   By: likong <likong@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 16:16:06 by likong            #+#    #+#             */
-/*   Updated: 2024/06/26 13:54:59 by likong           ###   ########.fr       */
+/*   Updated: 2024/08/20 09:58:00 by likong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/so_long.h"
-
-bool	is_valid_move(t_game *g)
-{
-	if (is_same_point(g->curr, g->next))
-		return (false);
-	else if (is(g, g->next) == WALL)
-	{
-		ft_printf("Please don't banging wall.\n");
-		return (false);
-	}
-	else if (is_same_point(g->curr, g->exit)
-		&& g->coins != g->map->chests
-		&& is(g, g->next) != WALL)
-	{
-		g->map->cont[g->curr.y][g->curr.x] = EXIT;
-		g->map->cont[g->next.y][g->next.x] = PLAYER;
-		ft_printf("Moves: %u, coins: %u\n", ++g->moves, g->coins);
-		g->img[P2]->instances[0].x = g->next.x * g->tile;
-		g->img[P2]->instances[0].y = g->next.y * g->tile;
-		g->curr = g->next;
-		return (false);
-	}
-	return (true);
-}
 
 static void	check_chest(t_game *g)
 {
@@ -55,6 +31,43 @@ static void	check_chest(t_game *g)
 		g->img[E1]->instances[0].enabled = false;
 }
 
+static void	real_moving(t_game *g)
+{
+	g->map->cont[g->next.y][g->next.x] = PLAYER;
+	ft_printf("Moves: %u, coins: %u\n", ++g->moves, g->coins);
+	g->img[P2]->instances[0].x = g->next.x * g->tile;
+	g->img[P2]->instances[0].y = g->next.y * g->tile;
+	g->curr = g->next;
+}
+
+bool	is_valid_move(t_game *g)
+{
+	if (is_same_point(g->curr, g->next))
+		return (false);
+	else if (is(g, g->next) == WALL)
+	{
+		ft_printf("Please don't banging wall.\n");
+		return (false);
+	}
+	else if (is_same_point(g->curr, g->exit) && g->coins != g->map->chests
+		&& is(g, g->next) != WALL && is(g, g->next) != CHEST)
+	{
+		g->map->cont[g->curr.y][g->curr.x] = EXIT;
+		real_moving(g);
+		return (false);
+	}
+	else if (is_same_point(g->curr, g->exit) && g->coins != g->map->chests
+		&& is(g, g->next) != WALL && is(g, g->next) == CHEST)
+	{
+		g->coins++;
+		check_chest(g);
+		g->map->cont[g->curr.y][g->curr.x] = EXIT;
+		real_moving(g);
+		return (false);
+	}
+	return (true);
+}
+
 void	move_player(t_game *g)
 {
 	if (!is_valid_move(g))
@@ -71,9 +84,5 @@ void	move_player(t_game *g)
 		quit(g);
 	}
 	g->map->cont[g->curr.y][g->curr.x] = SPACE;
-	g->map->cont[g->next.y][g->next.x] = PLAYER;
-	ft_printf("Moves: %u, coins: %u\n", ++g->moves, g->coins);
-	g->img[P2]->instances[0].x = g->next.x * g->tile;
-	g->img[P2]->instances[0].y = g->next.y * g->tile;
-	g->curr = g->next;
+	real_moving(g);
 }
